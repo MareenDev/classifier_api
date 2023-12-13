869dc563d6d314db6a172f90cabbc5ef8d1e36b3
MareenDev <moinmoin1101@aol.com>
2023-12-13 15:21:34 +0100

    SetUp Webservice

--- src/api.py
+++ src/api.py
@@ -1,22 +1,56 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask_cors import CORS
+import json
+import src.helpers as helpers
+from src.dataPreparation import outputMapping
+
 
 app = Flask(__name__)
 CORS(app)
 
 
+paths = helpers.paths()
+prefix = "M_cnn_D_fmnist"  # "M_" + FLAGS.model + "_D_" + FLAGS.dataset
+filepath = paths.get_path_file_model(prefix)
+model = helpers.get_object_from_pkl(filepath)
+
+
 @app.route('/', methods=['GET'])
 def main():
-    return {
-        "hello": "world!!!123!!",
-    }
+    string = "Willkommen, schön, dass Sie hier sind! \n\r Für eine Klassifikation Ihres Bildes, nutzen Sie bitte einen Aufruf entsprechend unserer API siehe-> TBD"
+    result = Response(content_type='application/json')
+    result.set_data(string)
+
+    return result
 
 
-@app.route('/hello_world', methods=['GET'])
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/check', methods=['GET'])
+def ckeck():
+    result = Response(content_type='application/json')
+
+    if model is None: 
+        string = "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut!"
+    else:
+        string = "Der Service zur Klassifizierung Ihrer Bilder steht bereit."
+    result.set_data(string)
+
+    return result
 
 
-@app.route('/trainings_data', methods=['GET'])
-def get_trainingsdata():
-    return Response(content_type='application/json')
+@app.route('/classifiy', methods=['POST'])
+def predict():
+    result = Response(content_type='application/json')
+
+    if model is None: 
+        result.status_code = 500
+        string = "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später erneut!"
+    else: 
+        model_input = helpers.convertWSRequestToTensor(json.loads(request.data))
+        prediction = model(model_input)
+        _, label = prediction.max(1)
+        label = label.item()
+        string = "{\"label\":"+outputMapping(ds='fmnist', label=label)+"}"
+
+    result.set_data(string)
+
+    return result
